207288f2f253c728df1c3c6aab05a1a556c662c9
Ahmed Alli <muniirhaddadi1@gmail.com>
2022-07-16 23:37:15 +0300

    Creates a function that output the weekly and yearly incomes

--- assets/js/index.js
+++ assets/js/index.js
@@ -5,15 +5,51 @@ function totalProduction() {
     var shed3 = parseInt(document.getElementById("input3").value);
     var shed4 = parseInt(document.getElementById("input4").value);
     var total = shed1 + shed2 + shed3 + shed4 ;
-    let str = "your production in shed A is " + " " +shed1 +" "+ "litres per day <br>";
-    str += "your production in shed B is " + " " +shed2 +" "+ "litres per day <br>";
-    str += "your production in shed C is " + " " +shed3 +" "+ "litres per day <br>";
-    str += "your production in shed D is " + " " +shed4 +" "+ "litres per day <br>";
-    str += "your total production is "+" " +total +" " +"litres per day";
+    let str = "<h4> Summary of production per day</h4> <br>";
+    str += "your production in shed A is " + shed1 + " litres per day <br>";
+    str += "your production in shed B is " + shed2 + " litres per day <br>";
+    str += "your production in shed C is " + shed3 + " litres per day <br>";
+    str += "your production in shed D is " + shed4 + " litres per day <br>";
+    str += "your total production is " + total + " litres per day";
 
     document.getElementById("display").innerHTML = str;
 }
 
+  
+
+function calculateIncome(){
+    var shed1 = parseInt(document.getElementById("input1").value);
+    var shed2 = parseInt(document.getElementById("input2").value);
+    var shed3 = parseInt(document.getElementById("input3").value);
+    var shed4 = parseInt(document.getElementById("input4").value);
+
+    let price =parseInt(document.getElementById("input5").value);
+    var total = shed1 + shed2 + shed3 + shed4 ;
+
+        var selectedOption = document.getElementsByName('choice')
+            if(document.getElementById("weekly").checked){
+
+            let income  = 7 * price * total;
+            let str = "your weekly income will be ksh " + income;
+
+            document.getElementById("display").innerHTML = str; 
+
+         }
+
+         else {
+            let income = 365 * price * total;
+            let str = "your yearly income will be ksh " + income;
+
+
+            document.getElementById("display").innerHTML = str;
+         }
+    
+    
+         
+
+      
+    
+}
   
  
 
